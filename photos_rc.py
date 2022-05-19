6caf5006e5ce8a2f06863977d58e2673ed4791f0
Aude Marie KOUASSY <93558050+Keldy7@users.noreply.github.com>
2022-05-19 01:04:45 +0000

    Update photos_rc.py

--- photos_rc.py
+++ photos_rc.py
@@ -1,5 +1,5 @@
 # Resource object code (Python 3)
-# Created by: object code
+# Created by: Aude Marie KOUASSY
 # Created by: The Resource Compiler for Qt version 5.15.2
 # WARNING! All changes made in this file will be lost!
 
